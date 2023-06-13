b6ecbc41cda67b84fd5ff797395b9dd17f43e309
BuildTools <unconfigured@null.spigotmc.org>
2023-06-13 12:57:01 +0200

    ADD, Plant form component :sparkles:

---
+++ plantswap/components/Form/Form.js
@@ -0,0 +1,116 @@
+'use client'
+
+import React from 'react';
+
+import styles from "./form.module.scss";
+
+const Form = ({ data }) => {
+
+
+    console.log(data);
+    
+    return (
+        <main>
+
+            <section>
+
+                <form className={styles.form}>
+
+                    <fieldset className={styles.form__fieldset}>
+
+                        <legend className={styles.form__legend}><span>1.</span>Groene Gegevens</legend>
+
+                        <div className={styles.form__input_group}>
+                            <label className={styles.form__label} htmlFor="plantNaam">Naam van de plant</label>
+                            <span id="plantNaamDesc" className={styles.form__label_description}>Vul hier de naam van de plant in.</span>
+                            <input className={styles.form__input_text} type="text" id="plantNaam" aria-describedby="plantNaamDesc" placeholder="Aziatische Lelies"/>
+                        </div>
+
+                        <div className={styles.form__input_group}>
+                            <label className={styles.form__label} htmlFor="plantHerkomst">Herkomst van de plant</label>
+                            <span id="plantHerkomstDesc" className={styles.form__label_description}>Vul hier de herkomst van de plant in.</span>
+                            <select id="plantHerkomst" className={styles.form__input_select} aria-describedby="plantHerkomstDesc">
+                                <option value="afrika">Afrika</option>
+                                <option value="antartica">Antartica</option>
+                                <option value="azie">Azië</option>
+                                <option value="australie">Australië</option>
+                                <option value="europa">Europa</option>
+                                <option value="noordAmerika">Noord Amerika</option>
+                                <option value="zuidAmerika">Zuid Amerika</option>
+                            </select>
+                        </div>
+                    </fieldset>
+
+                    <fieldset className={styles.form__fieldset}>
+
+                        <legend className={styles.form__legend}><span>2.</span>Onderhoud</legend>
+
+                        <div className={styles.form__input_group}>
+                            <label className={styles.form__label} htmlFor="onderhoudMoeilijkheidsgraad">Moeilijkheidsgraad onderhoud</label>
+                            <span id="onderhoudDesc" className={styles.form__label_description}>Vul hier de moeilijkheidsgraad van de plant in.</span>
+                            <select id="onderhoudMoeilijkheidsgraad" className={styles.form__input_select} aria-describedby="onderhoudDesc">
+                                <option value="nietMoeilijk">Niet moeilijk te onderhouden</option>
+                                <option value="gemiddeld">Vereist wat ervaring om te onderhouden</option>
+                                <option value="heelMoeilijk">Heel moeilijk te onderhouden</option>
+                            </select>
+                        </div>
+                    </fieldset>
+
+                    <fieldset className={styles.form__fieldset}>
+                        <legend className={styles.form__legend}><span>3.</span>Verzorging</legend>
+
+                        <div className={styles.form__input_group}>
+                            <label className={styles.form__label} htmlFor="waterBehoefte">Behoefte aan water</label>
+                            <span id="waterDesc" className={styles.form__label_description}>Vul hier de waterbehoefte van de plant in.</span>
+                            <div className={styles.form__input_water_interval_group}>
+                                <input type="text" id="waterBehoefte" className={styles.form__input_text} aria-describedby="waterDesc" placeholder="1"/>
+                                <span>X per</span>
+                                <select id="waterInterval" className={styles.form__input_select}>
+                                    <option value="">Standaard</option>
+                                    <option value="perDag">Per dag</option>
+                                    <option value="perWeek">Per week</option>
+                                    <option value="perMaand">Per maand</option>
+                                    <option value="perJaar">Per jaar</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div className={styles.form__input_group}>
+                            <label className={styles.form__label} htmlFor="voedingBehoefte">Behoefte aan voeding</label>
+                            <span id="voedingDesc" className={styles.form__label_description}>Vul hier de behoefte aan voeding van de plant in.</span>
+                            <input type="text" id="voedingBehoefte" className={styles.form__input_text} aria-describedby="voedingDesc" placeholder="Om de twee maanden, plantvoeding."/>
+                        </div>
+                    </fieldset>
+
+                    <fieldset className={styles.form__fieldset}>
+                        <legend className={styles.form__legend}><span>4.</span>Giftig</legend>
+
+                        <div className={styles.form__input_group}>
+                            <label className={styles.form__label}>Is deze plant giftig?</label>
+                            <span id="giftigDesc" className={styles.form__label_description}>Vink aan als deze plant giftig is.</span>
+
+                            <div className={styles.form__input_checkbox_wrapper}>
+                                <input type="checkbox" className={styles.form__input_checkbox} id="giftigCheckbox" aria-describedby="giftigDesc"/>
+                                <label className={styles.form__input_checkbox_label} htmlFor="giftigCheckbox">Ja, deze plant is giftig.</label>
+                            </div>
+
+                            <div className={styles.form__input_sub_group}>
+                                <label className={styles.form__label} htmlFor="giftigAddon">Aanvulling</label>
+                                <span id="giftigAddonDesc" className={styles.form__label_description}>Aanvullende beschrijving.</span>
+                                <input type="text" id="giftigAddon" className={styles.form__input_text} aria-describedby="giftigAddon" placeholder="Deze plant kan giftig zijn voor ..."/>
+                            </div>
+
+                        </div>
+                    </fieldset>
+
+                    <button type="submit" className={styles.form__submit_button}>Volgende stap</button>
+                </form>
+
+            </section>
+
+
+        </main >
+    );
+};
+
+export default Form;
--- plantswap/src/app/[lang]/planten/[id]/page.js
+++ plantswap/src/app/[lang]/planten/[id]/page.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import Title from "../../../../../components/Title/Title.js";
 import Detailpage from "../../../../../components/Detailpage/Detailpage.js";
-// import DOMParser from "dom-parser";
+
+import Form from "../../../../../components/Form/Form.js";
+
+import dataset from "../../swap/data.json";
 
 import { graphql, buildSchema } from "graphql";
 import Layout from "../../../../../components/Layout/Base.js";
@@ -9,82 +12,10 @@ import { getDictionary } from "../../../../../get-dictionary.js";
 
 export default async function Home({ params }) {
   const dictionary = await getDictionary(params.lang);
-  const revalidateTime = 60; // time in seconds
-  const endpoint = "https://trefle.io";
-  const apiKey = process.env.NEXT_PUBLIC_API_KEY2;
 
-  var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-  var rootValue = { hello: () => "Hello world!" };
-
-  var source = "{ hello }";
-
-  graphql({ schema, source, rootValue }).then((response) => {
-    console.log(response);
-  });
-
-  const query = `query MyQuery {
-    plants(locales: nl) {
-      id
-      name
-    }
-  }`;
-
-  const endpointHygraph = `https://eu-central-1-shared-euc1-02.cdn.hygraph.com/content/clij3imw706zl01uk9hgh5uw6/master?query=${query}`;
-  // const endpointHygraph = `https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clij3imw706zl01uk9hgh5uw6/master?query=${query}`;
-  const data = () => {
-    return fetch(endpointHygraph, {
-      headers: {
-        Autorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ3ZWJhcHAiOnRydWUsInZlcnNpb24iOjQsImlhdCI6MTY4NTk4NDM2NSwiZXhwIjoxNjg2MDI3NTY1LCJhdWQiOlsiaHR0cHM6Ly9hcGktZXUtY2VudHJhbC0xLXNoYXJlZC1ldWMxLTAyLmh5Z3JhcGguY29tL3YyL2NsaWozaW13NzA2emwwMXVrOWhnaDV1dzYvbWFzdGVyIl0sImlzcyI6Imh0dHBzOi8vbWFuYWdlbWVudC5ncmFwaGNtcy5jb20vIiwic3ViIjoiY2NmYjIxOWUtNTY0YS00MDA1LTgwZGMtODJlNjRjMmEwOGFkIiwianRpIjoiY2xpajNoYnU2MDZqOTAxdDY3cmEzZnFraCJ9.5UA1cnfEumcltU1fZROgJnKJnePW5Uar9CnrId0CKrymF_YiGgTI87auVhdpz-M91Qeno157osXD09Dm2-8xs-jSL3qDvNSztIAiGQWRUMwaQzgjyFCfGrtajJsuex_nIXinvApJaQQNs50FnvHTJsQaUnLupjE5_8NIBJH1zQqLzzLa3biqmSoaG7e9L0Ph_mqhbq8QPnMHxIKYGWf07nwav9y2CmJgtiVnFsri6qoPaIJ5xqprvcR--cfru_vGRE5mSY6J7W1-fMBpL-d2pyHfzSTlUeRbUn7Ste6RTYeB30NCtCHBL2qBJmosY86mmDP0dsuXQEk-BbyM0aPzh3HAWVm0Snt2bXIJ8KizXxEasK7PLgN15yH_Qqftaw8wnlaC8lbGaevj-fEru5YZvdw7RsKThYotU8NdY-mSD8oUYheN9aO0BoC0z0hJjQZWhiSjRtz0Y6ArlUXyEYHkdrgAMIzCyhGVt8yxESj3uVrSdS7Hteq6Zz7sinWja7jJILeBfppJoa2ZqvXkx4RsLrj-CZkNOzdyFkDbTaf_M1EfrGeGPRvFLZA0i3_86YA1ByudZTTb_B5DuG3W6C8IqrCsKlcFwgyd8DGMDvCFm6k8M5pq_schfs7RL8VVyzZhAZB1hAezQ6Msat6DupbB0xP1Uhl8nqquG2p-1fcSVz4`,
-      },
-    })
-      .then((res) => res.json())
-      .catch((err) => console.log(err));
-  };
-
-  // query()
-  console.log((await data()).data);
-
-  const fetchedData = await fetch(
-    `${endpoint}/api/v1/plants/search?token=${apiKey}&q=${params.id}`,
-    {
-      next: { revalidate: revalidateTime },
-    }
-  ).then((res) => res.json());
-
-  console.log(apiKey);
-
-  // console.log(await fetchedData);
-
-  const items = await fetch(
-    `${endpoint}${await fetchedData.data[0]?.links?.plant}?token=${apiKey}`,
-    {
-      next: { revalidate: revalidateTime },
-    }
-  )
-    .then((res) => res.json())
-    .catch((err) => {
-      return { err };
-    });
-
-  if (items.err)
-    // Error page
-    return (
-      <Layout dictionary={dictionary} locale={params.lang}>
-        <Title title="h1">Planten soort {params.id} niet gevonden</Title>
-      </Layout>
-    );
-
-  // console.log(items);
-  // Detail page
   return (
     <Layout dictionary={dictionary} locale={params.lang}>
-      <Detailpage data={items} />
+      <Form data={dataset} />
     </Layout>
   );
-  return <Title title="h1">Hier komt de {params.id}</Title>;
 }
