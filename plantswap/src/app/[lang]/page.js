98c3b85c0766f6e461456ab23fd88c3dd8feb28b
Stefan Radouane <stefan.radouane@hva.nl>
2023-06-30 11:01:15 +0200

    :bug: FIX sparkles

--- plantswap/src/app/[lang]/page.js
+++ plantswap/src/app/[lang]/page.js
@@ -1,35 +1,34 @@
-import styles from './page.module.scss';
-import Title from '@/../components/Title/Title';
-import Hero from '@/../components/Hero/Hero';
-import { getDictionary } from '@/../get-dictionary';
-import Overview from '@/../components/Overview/Overview';
-import Layout from '../../../components/Layout/Base';
-import { getData } from '../../../utils/getFormData';
+import styles from "./page.module.scss";
+import Title from "@/../components/Title/Title";
+import Hero from "@/../components/Hero/Hero";
+import { getDictionary } from "@/../get-dictionary";
+import Overview from "@/../components/Overview/Overview";
+import Layout from "../../../components/Layout/Base";
+import { getData } from "../../../utils/getFormData";
 
 export default async function Page({ params }) {
-	const dictionary = await getDictionary(params.lang);
-	const data = await getData(params.lang);
+  const dictionary = await getDictionary(params.lang);
+  const data = await getData(params.lang);
 
-	return (
-		<Layout locale={params.lang} dictionary={dictionary}>
-			<main className={styles.page}>
-				<Hero
-					dictionary={dictionary.homepage}
-					lang={params.lang}
-					image={'/images/hero.png'}
-					linkTo={`/${params.lang}/swap`}
-				/>
-				<section className={styles.page__overview} id="overview-heading">
-					<Title
-						title={'h1'}
-						modifier={'gentle-appear'}
-						className={styles['page--subTitle']}
-					>
-						{dictionary.homepage.subTitle}
-					</Title>
-					<Overview formInfo={data.plantforms} data={data.plants} />
-				</section>
-			</main>
-		</Layout>
-	);
+  return (
+    <Layout locale={params.lang} dictionary={dictionary}>
+      <main className={styles.page}>
+        <Hero
+          dictionary={dictionary.homepage}
+          lang={params.lang}
+          image={"/images/hero-image.png"}
+          linkTo={`/${params.lang}/swap`}
+        />
+        <section className={styles.page__overview} id="overview-heading">
+          <Title
+            title={"h1"}
+            modifier={"gentle-appear"}
+            className={styles["page--subTitle"]}>
+            {dictionary.homepage.subTitle}
+          </Title>
+          <Overview formInfo={data.plantforms} data={data.plants} />
+        </section>
+      </main>
+    </Layout>
+  );
 }
